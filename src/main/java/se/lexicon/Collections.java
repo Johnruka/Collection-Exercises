3dfc1e066b5ac31a416ca33426fc5183c418dc97
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-08 12:37:50 +0200

    Created

---
+++ src/main/java/se/lexicon/Collections.java
@@ -0,0 +1,26 @@
+package se.lexicon;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collections {
+    public static void main(String[] args) {
+
+    }
+
+    public static void ex1() {
+        List<String> daysOfTheWeek = new ArrayList<>();
+        daysOfTheWeek.add("Monday");
+        daysOfTheWeek.add("Tuesday");
+        daysOfTheWeek.add("Wednesday");
+        daysOfTheWeek.add("Thursday");
+        daysOfTheWeek.add("Friday");
+        daysOfTheWeek.add("Saturday");
+        daysOfTheWeek.add("Sunday");
+
+
+
+
+    }
+}
